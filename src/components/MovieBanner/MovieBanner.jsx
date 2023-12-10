614efc38ca8a7f7a70232338867d679701b3a8e5
Vinicius <naoprefiro@gmail.com>
2023-12-10 18:05:37 -0300

    mobile

--- src/components/MovieBanner/MovieBanner.jsx
+++ src/components/MovieBanner/MovieBanner.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import styles from './movieBanner.module.css'
+import { RxDividerVertical } from "react-icons/rx";
+
 export const MovieBanner = ({image,movie}) => {
   console.log(movie)
   console.log(movie.genres[0].name)
   return (
     <section className={styles.movieBanner} style={{backgroundImage:`url("${image}")`}}>
         <div className={styles.information}>
-
+          <div className={styles.subheader}>
            <div className={styles.genres}>
             {movie.genres.map((genre)=>{
               console.log(genre.name)
@@ -15,9 +17,13 @@ export const MovieBanner = ({image,movie}) => {
               )
             })}
            </div>
-
+            <RxDividerVertical></RxDividerVertical>
+             {movie.vote_average.toFixed(1)}
+          </div>
             <h2>{movie.title}</h2>
-            <p>{movie.tagline}</p>
+            <p>{movie.tagline} </p>
+
+            
         </div>
     </section>
   )
